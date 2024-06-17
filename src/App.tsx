471b881637a8889ebdf15b026d2cf7cadc13c40d
Amy Kim <kimmaha9902@gmail.com>
2024-06-17 19:46:22 +0900

    Profile page updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './pages/Main';
 import ErrorPage from './pages/ErrorPage';
 import Login from './pages/Login';
-import Carinfo from './pages/CarInfo';
+import CarInfo from './pages/CarInfo';
 import Recommend from './pages/Recommend';
-import Profile from './pages/Profile';
+import { ProfileForm } from './pages/Profile';
 import Account from './pages/Account';
 import Activity from './pages/Activity';
+import LayoutUsers from './pages/LayoutUsers';
 import ChargerMap from './pages/ChargerMap';
 
 const router = createBrowserRouter([
@@ -29,19 +30,25 @@ const router = createBrowserRouter([
   },
   {
     path: '/carinfo',
-    element: <Carinfo />,
+    element: <CarInfo />,
   },
   {
-    path: '/setting/profile',
-    element: <Profile />,
-  },
-  {
-    path: '/setting/account',
-    element: <Account />,
-  },
-  {
-    path: '/setting/activity',
-    element: <Activity />,
+    path: '/setting',
+    element: <LayoutUsers />, // LayoutUsers가 부모 컴포넌트
+    children: [
+      {
+        path: 'profile',
+        element: <ProfileForm />, // Profile 자식 컴포넌트
+      },
+      {
+        path: 'account',
+        element: <Account />,
+      },
+      {
+        path: 'activity',
+        element: <Activity />,
+      },
+    ],
   },
 ]);
 
--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -9,3 +9,30 @@ function Account() {
 }
 
 export default Account;
+
+// import React, { useState }  from 'react';
+// import { zodResolver } from '@hookform/resolvers/zod';
+// import { useFieldArray, useForm } from 'react-hook-form';
+// import { z } from 'zod';
+// import { Link } from 'react-router-dom';
+// import { Button } from '../../components/ui/button';
+// import {
+//   Form,
+//   FormControl,
+//   FormDescription,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from '../../components/ui/form';
+// import { Input } from '../../components/ui/input';
+// import { cn } from '../../lib/utils'; 
+// import { Separator } from '../../components/ui/separator'; 
+
+
+// const EmailSchema = z.object({
+//   email: z
+//     .string()
+//     .email({ message: '유효한 이메일 주소를 입력해 주세요.' })
+//     .max(100, { message: '이메일 주소는 최대 100자 이하로 입력해 주세요.' }),
+// })
