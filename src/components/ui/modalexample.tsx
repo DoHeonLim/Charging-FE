bee23c589da653317361ebf913cc7cd1871f94ee
[AI10] 김재근 <jskim331933@naver.com>
2024-06-19 00:51:05 +0900

    feat:반복 컴포넌트 함수화, svg 경로 수정, modal을 이용한 상세보기 추가

--- src/components/ui/modalexample.tsx
+++ src/components/ui/modalexample.tsx
@@ -2,29 +2,19 @@
 import Modal from 'react-modal';
 import { useAtomValue, useAtom } from 'jotai';
 import { carAtom, openAtom } from '@/atoms/car';
-// import { Car } from '@/types/car';
 import carImage from '@/assets/images/d.png';
+import { Button } from '@/components/ui/button';
 
 import {
   MinimalCard,
-  MinimalCardDescription,
-  MinimalCardImage,
-  MinimalCardTitle,
+  // MinimalCardDescription,
+  // MinimalCardImage,
+  // MinimalCardTitle,
 } from '@/components/ui/minimal-card';
 
 const ModalExample = () => {
-  //   const setSelectCar = useSetAtom(carAtom);
   const selectCar = useAtomValue(carAtom);
   const [modalIsOpen, setModalIsOpen] = useAtom(openAtom);
-
-  //   const onCliclickSelectCar = useCallback((car: Car) => {
-  //     openModal;
-  //     setSelectCar(car);
-  //   }, []);
-
-  //   const openModal = () => {
-  //     setModalIsOpen(true);
-  //   };
 
   const closeModal = () => {
     setModalIsOpen(false);
@@ -32,20 +22,28 @@ const ModalExample = () => {
 
   return (
     <div>
-      {/* <button onClick={(car) => onCliclickSelectCar}>모달 열기</button> */}
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
-        <MinimalCard>
-          <MinimalCardImage src={carImage} alt={selectCar ? selectCar.name : '빈값'} />
-          <MinimalCardTitle>
-            {selectCar ? selectCar.brand + ' ' + selectCar.name : '빈값'}
-          </MinimalCardTitle>
-          <MinimalCardDescription>
-            {selectCar ? selectCar.model_year : '빈값'} | 복합전비 :
-            {selectCar ? selectCar.fuel_efficiency : '빈값'} ㎞/kWh |
-            {selectCar ? selectCar.car_type : '빈값'}
-          </MinimalCardDescription>
-        </MinimalCard>
-        <button onClick={closeModal}>닫기</button>
+      <Modal
+        isOpen={modalIsOpen}
+        ariaHideApp={false}
+        onRequestClose={closeModal}
+        className='top-1/2 left-1/2'
+      >
+        <div className='grid grid-cols-4 gap-4'>
+          <MinimalCard>
+            <img
+              src={carImage}
+              alt={selectCar ? selectCar.name : ''}
+              className='w-[300px] h-[200px]'
+            />
+          </MinimalCard>
+          <div>{selectCar ? selectCar.brand + ' ' + selectCar.name : ''}</div>
+          <div>
+            {selectCar ? selectCar.model_year : ''} | 복합전비 :
+            {selectCar ? selectCar.fuel_efficiency : ''} ㎞/kWh |
+            {selectCar ? selectCar.car_type : ''}
+          </div>
+          <Button onClick={closeModal}>닫기</Button>
+        </div>
       </Modal>
     </div>
   );
--- src/pages/CarInfo.tsx
+++ src/pages/CarInfo.tsx
@@ -1,3 +1,4 @@
+//import component
 import Layout from '@/components/Layout/Layout';
 import {
   MinimalCard,
@@ -5,55 +6,57 @@ import {
   MinimalCardImage,
   MinimalCardTitle,
 } from '@/components/ui/minimal-card';
-import carImage from '../assets/images/d.png';
 import { Dock, DockIcon } from '@/components/ui/dock';
-import Icons from '@/assets/images/car-logo/car-logo';
 import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card';
-import { useState, useCallback } from 'react';
-import { carData } from '@/data/car';
-// import Base from '@/components/Carboard/Base';
 import ModalExample from '@/components/ui/modalexample';
-// import Modal from 'react-modal';
+import { Button } from '@/components/ui/button';
+//import data
+import { carData } from '@/data/car';
+//import type & atom
 import { Car } from '@/types/car';
 import { carAtom, openAtom } from '@/atoms/car';
-import { useSetAtom } from 'jotai';
-// export type IconProps = React.HTMLAttributes<SVGElement>;
+import { useSetAtom, useAtom } from 'jotai';
+//import image
+import carImage from '../assets/images/d.png';
+import KoreanCarIcons from '@/assets/images/car-logo/car-logo';
+import GlobalCarIcons from '@/assets/images/car-logo/car-logo2';
+//import function
+import { useState, useCallback, useEffect } from 'react';
 
 function CarInfo() {
   const [selectedBrand, setSelectedBrand] = useState('현대');
   const setModalIsOpen = useSetAtom(openAtom);
-  // const selectCar = useAtomValue(carAtom);
-  const setSelectCar = useSetAtom(carAtom);
+  const [selectCar, setSelectCar] = useAtom(carAtom);
 
   const onClickSelectCar = useCallback((car: Car) => {
     setSelectCar(car);
     setModalIsOpen(true);
-    console.log(openAtom), console.log(carAtom);
   }, []);
 
-  // const openModal = () => {
-  //   setModalIsOpen(true);
-  // };
-
-  // const closeModal = () => {
-  //   setModalIsOpen(false);
-  // };
+  useEffect(() => {
+    setSelectCar(selectCar);
+  }, [selectCar]);
 
   function Reveal() {
     const filteredBrandCars = carData[0].cars.filter((car) => car.brand === selectedBrand);
     return (
       filteredBrandCars &&
-      filteredBrandCars.map((car: Car) => (
-        <div>
-          <MinimalCard key={car.id} onClick={(car) => onClickSelectCar}>
+      filteredBrandCars.map((car: Car, idx) => (
+        <div key={idx}>
+          <MinimalCard key={car.id} onClick={() => onClickSelectCar(car)}>
             {/* //car */}
             <MinimalCardImage src={carImage} alt={car.name} />
             <MinimalCardTitle>{car.brand + ' ' + car.name}</MinimalCardTitle>
             <MinimalCardDescription>
               {car.model_year} | 복합전비 : {car.fuel_efficiency} ㎞/kWh | {car.car_type}
             </MinimalCardDescription>
+            <Button
+              onClick={() => onClickSelectCar(car)}
+              className='w-[64px] h-[48px] hover:bg-yellow-600/75'
+            >
+              상세보기
+            </Button>
           </MinimalCard>
-          <ModalExample />
         </div>
       ))
     );
@@ -65,123 +68,18 @@ function CarInfo() {
       <HoverCard openDelay={0.2} closeDelay={0.2}>
         <HoverCardTrigger asChild>
           <Dock>
-            <DockIcon>
-              <Icons.hyundai
-                className='h-6 w-6'
+            {KoreanCarIcons.map((item) => (
+              <div
                 onClick={() => {
-                  setSelectedBrand('현대');
+                  setSelectedBrand(item.name);
                   Reveal();
                 }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.kia
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('기아');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.genesis
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('제네시스');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.kgm
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('KGM');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.daechang
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('대창 모터스');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.jeis
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('제이스모빌리티');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.dpeco
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('디피코');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.cevo
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('쎄보');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.maiv
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('마이브');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.evkmc
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('EVKMC');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.smartev
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('스마트');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.evion
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('이비온');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.mobilitynetworks
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('모빌리티네트웍스');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
+              >
+                <DockIcon>
+                  <img src={item.src} className='w-[36px] h-[36px]' />
+                </DockIcon>
+              </div>
+            ))}
           </Dock>
         </HoverCardTrigger>
         <HoverCardContent side='top' className='flex justify-center'>
@@ -191,177 +89,18 @@ function CarInfo() {
       <HoverCard openDelay={0.2} closeDelay={0.2}>
         <HoverCardTrigger asChild>
           <Dock>
-            <DockIcon>
-              <Icons.bmw
-                className='h-6 w-6'
+            {GlobalCarIcons.map((item) => (
+              <div
                 onClick={() => {
-                  setSelectedBrand('BMW');
+                  setSelectedBrand(item.name);
                   Reveal();
                 }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.benz
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('벤츠');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.audi
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('아우디');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.tesla
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('TESLA');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.volkswagen
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('폭스바겐');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.byd
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('BYD');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.lotus
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('로터스');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.polestar
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('폴스타');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.volvo
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('볼보');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.lexus
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('렉서스');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.lucid
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('루시드');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.rivian
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('리비안');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.dongfeng
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('동풍');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.rollsroyce
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('롤스로이스');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.porsche
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('포르쉐');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.ford
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('포드');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.jeep
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('지프');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.peugeot
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('푸조');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
-            <DockIcon>
-              <Icons.ds
-                className='h-6 w-6'
-                onClick={() => {
-                  setSelectedBrand('DS3');
-                  Reveal();
-                }}
-              />
-            </DockIcon>
+              >
+                <DockIcon>
+                  <img src={item.src} className='w-[36px] h-[36px]' />
+                </DockIcon>
+              </div>
+            ))}
           </Dock>
         </HoverCardTrigger>
         <HoverCardContent className='flex justify-center'>해외 전기차 브랜드</HoverCardContent>
@@ -370,6 +109,7 @@ function CarInfo() {
         <div className='min-h-[100px] p-4 rounded-lg'>
           <div className='w-[300px] grid grid-cols-1 sm:grid-cols-2 sm:w-[600px] lg:grid-cols-3 lg:w-[1000px] gap-4'>
             <Reveal />
+            <ModalExample />
           </div>
         </div>
       </div>
